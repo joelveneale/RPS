9ff88040318a2cc092b47a79d80aba4fb7f965c1
joelveneale <joelpneale@gmail.com>
2020-04-06 14:38:16 +0100

    first fully functioning version

--- RPS.py
+++ RPS.py
@@ -6,46 +6,47 @@ rock = 0
 paper = 1
 scissors = 2
 
-gamecount = input('How many games shall we play?')
+gamecount = int(input('How many games shall we play?'))
 
 
 while gamecount > 0:
 
   minmax = [0, 1, 2]
 
-  computers_choice = random.randit(0, 2)
-  users_choice = input("Roshambo!! Press 0 for Rock, 1 for Paper and 2 for Scissors!! ")
 
-  if computers_choice == 0 & users_choice == 0:
+  users_choice = int(input("Roshambo!! Press 0 for Rock, 1 for Paper and 2 for Scissors!! "))
+  computers_choice = random.randint(0, 2)
+
+  if computers_choice == 0 and users_choice == 0:
       print("You both threw ROCK, go again... ")
-  elif computers_choice == 1 & users_choice == 1:
+  elif computers_choice == 1 and users_choice == 1:
       print("You both threw PAPER, go again... ")
-  elif computers_choice == 2 & users_choice == 2:
+  elif computers_choice == 2 and users_choice == 2:
       print("You both threw SCISSORS, go again... ")
 
-  elif computers_choice == 0 & users_choice == 1:
+  elif computers_choice == 0 and users_choice == 1:
       print("PAPER COVERS ROCK -- YOU WIN!")
       gamecount = gamecount - 1
-  elif computers_choice == 0 & users_choice == 2:
+  elif computers_choice == 0 and users_choice == 2:
       print("ROCK SMASHES SCISSORS -- YOU LOSE :( ")
       gamecount = gamecount - 1
-  elif computers_choice == 1 & users_choice == 0:
+  elif computers_choice == 1 and users_choice == 0:
       print("PAPER COVERS ROCK -- YOU LOSE :( ")
       gamecount = gamecount - 1
-  elif computers_choice == 1 & users_choice == 2:
+  elif computers_choice == 1 and users_choice == 2:
       print("SCISSORS CUT PAPER -- YOU WIN!")
       gamecount = gamecount - 1
-  elif computers_choice == 2 & users_choice == 0:
+  elif computers_choice == 2 and users_choice == 0:
       print("ROCK SMASHES SCISSORS -- YOU WIN!")
       gamecount = gamecount - 1
-  elif computers_choice == 2 & users_choice == 1:
+  elif computers_choice == 2 and users_choice == 1:
       print("SCISSORS CUT PAPER -- YOU LOSE :( ")
       gamecount = gamecount - 1
-  else:
-      users_choice not in minmax:
+  elif users_choice not in minmax:
       print("Press 0 for Rock, 1 for Paper and 2 for Scissors ")
 
 
+print("GAME OVER")
 
 
 
