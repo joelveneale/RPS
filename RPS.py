b2707ce4118c48ac1f86e95e99716eee16b6eea9
joelveneale <joelpneale@gmail.com>
2020-04-06 14:43:36 +0100

    new functionality for counting wins/loses

--- RPS.py
+++ RPS.py
@@ -7,6 +7,8 @@ paper = 1
 scissors = 2
 
 gamecount = int(input('How many games shall we play?'))
+user_wins = 0
+comp_wins = 0
 
 
 while gamecount > 0:
@@ -27,26 +29,39 @@ while gamecount > 0:
   elif computers_choice == 0 and users_choice == 1:
       print("PAPER COVERS ROCK -- YOU WIN!")
       gamecount = gamecount - 1
+      user_wins = user_wins + 1
   elif computers_choice == 0 and users_choice == 2:
       print("ROCK SMASHES SCISSORS -- YOU LOSE :( ")
       gamecount = gamecount - 1
+      comp_wins = comp_wins + 1
   elif computers_choice == 1 and users_choice == 0:
       print("PAPER COVERS ROCK -- YOU LOSE :( ")
       gamecount = gamecount - 1
+      comp_wins = comp_wins + 1
   elif computers_choice == 1 and users_choice == 2:
       print("SCISSORS CUT PAPER -- YOU WIN!")
       gamecount = gamecount - 1
+      user_wins = user_wins + 1
   elif computers_choice == 2 and users_choice == 0:
       print("ROCK SMASHES SCISSORS -- YOU WIN!")
       gamecount = gamecount - 1
+      user_wins = user_wins + 1
   elif computers_choice == 2 and users_choice == 1:
       print("SCISSORS CUT PAPER -- YOU LOSE :( ")
       gamecount = gamecount - 1
+      comp_wins = comp_wins + 1
   elif users_choice not in minmax:
       print("Press 0 for Rock, 1 for Paper and 2 for Scissors ")
 
 
 print("GAME OVER")
+print("You won " + str(user_wins) + " times & the computer won " + str(comp_wins) + " times:")
+if user_wins > comp_wins:
+    print("YOU ARE THE KING")
+if user_wins < comp_wins:
+    print("YOU SUCK DICK")
+if user_wins == comp_wins:
+    print("IT WAS A DRAW. HOW BORING")
 
 
 
